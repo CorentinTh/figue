f1cb2c5bde23cf8c10d0bdb6e4beb542f7823f83
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-04-02 15:19:43 +0200

    feat(figue): export buildConfigSchema

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { defineConfig } from './figue';
+export { buildConfigSchema, defineConfig } from './figue';
 export type { ConfigDefinition, ConfigDefinitionElement, ConfigDefinitionObject, EnvRecord } from './figue.types';
