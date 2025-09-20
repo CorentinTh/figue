49303c99e5852af9152aea3d095834c44e36dc20
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-09-21 01:37:08 +0200

    refactor(lint): added type-aware linting

--- src/figue.errors.ts
+++ src/figue.errors.ts
@@ -1,4 +1,5 @@
 import type { ConfigIssue } from './figue.types';
+import { castArray } from './utils';
 
 export function createFigueError({ message, code }: { message: string; code: string }) {
   const error = Object.assign(new Error(message), { code, isFigueError: true });
@@ -10,8 +11,14 @@ export function isFigueError(error: unknown): error is Error & { isFigueError: t
   return error instanceof Error && (error as Error & { isFigueError?: boolean }).isFigueError === true;
 }
 
+function serializeIssue({ message, path, definition }: ConfigIssue) {
+  const envKey = definition?.env !== undefined ? castArray(definition.env).join(', ') : undefined;
+
+  return `${path?.join('.')}${envKey !== undefined ? ` (${envKey})` : ''}: ${message}`;
+}
+
 export function createConfigValidationError({ issues }: { issues: ReadonlyArray<ConfigIssue> }) {
-  const message = issues.map(({ path, message, definition }) => `${path?.join('.')}${definition?.env ? ` (${definition.env})` : ''}: ${message}`).join('\n');
+  const message = issues.map(serializeIssue).join('\n');
 
   return createFigueError({ message, code: 'CONFIG_VALIDATION_ERROR' });
 }
