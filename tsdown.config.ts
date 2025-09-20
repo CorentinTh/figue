0cfdb22830b52b64c385f9e3218e2718a8b52746
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-09-21 00:33:57 +0200

    refactor(build): ditched sourcemaps in dist

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  dts: { sourcemap: true },
-  sourcemap: true,
   clean: true,
   exports: true,
 });
